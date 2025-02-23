1e5d96c4c8e5c2e3a836e80c0885a4e7662a180a
krxnkos <thomaspullan07@gmail.com>
2025-02-23 14:10:32 +0000

    Refactor build process to create partials directory and enhance EJS rendering logic

--- build.js
+++ build.js
@@ -2,71 +2,47 @@ const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 
-const viewsDir = path.join(__dirname, 'views');
 const buildDir = path.join(__dirname, 'build');
-const staticDir = path.join(__dirname, 'static');
+const viewsDir = path.join(__dirname, 'views');
+const partialsDir = path.join(viewsDir, 'partials');
 
 // Create build directory if it doesn't exist
 if (!fs.existsSync(buildDir)) {
-  fs.mkdirSync(buildDir);
-  console.log(`Created build directory: ${buildDir}`);
+    fs.mkdirSync(buildDir);
 }
 
-// Copy partials directory to build directory
-const partialsDir = path.join(viewsDir, 'partials');
-const buildPartialsDir = path.join(buildDir, 'partials');
+// Create partials directory in build directory
+const buildPartialsDir = path.join(buildDir, 'views', 'partials');
+fs.mkdirSync(buildPartialsDir, { recursive: true });
 
-if (!fs.existsSync(buildPartialsDir)) {
-  fs.mkdirSync(buildPartialsDir);
-  console.log(`Created directory: ${buildPartialsDir}`);
-}
-
+// Copy partials to build directory
 fs.readdirSync(partialsDir).forEach(file => {
-  fs.copyFileSync(path.join(partialsDir, file), path.join(buildPartialsDir, file));
-  console.log(`Copied partial file: ${file}`);
+    const srcFile = path.join(partialsDir, file);
+    const destFile = path.join(buildPartialsDir, file);
+    fs.copyFileSync(srcFile, destFile);
+    console.log(`Copied partial file: ${file}`);
 });
 
 // Function to render EJS files
 function renderEjsFiles(srcDir, destDir) {
-  fs.readdirSync(srcDir).forEach(file => {
-    const srcPath = path.join(srcDir, file);
-    const destPath = path.join(destDir, file.replace('.ejs', '.html'));
+    fs.readdirSync(srcDir).forEach(file => {
+        const srcFile = path.join(srcDir, file);
+        const destFile = path.join(destDir, file);
 
-    if (fs.lstatSync(srcPath).isDirectory()) {
-      if (!fs.existsSync(destPath)) {
-        fs.mkdirSync(destPath);
-        console.log(`Created directory: ${destPath}`);
-      }
-      renderEjsFiles(srcPath, destPath);
-    } else if (path.extname(file) === '.ejs') {
-      const template = fs.readFileSync(srcPath, 'utf-8');
-      const html = ejs.render(template, {}, { root: viewsDir });
-      fs.writeFileSync(destPath, html);
-      console.log(`Rendered EJS file: ${srcPath} to ${destPath}`);
-    }
-  });
+        if (fs.lstatSync(srcFile).isDirectory()) {
+            fs.mkdirSync(destFile, { recursive: true });
+            renderEjsFiles(srcFile, destFile);
+        } else if (path.extname(file) === '.ejs') {
+            const template = fs.readFileSync(srcFile, 'utf-8');
+            const html = ejs.render(template, {}, { filename: srcFile });
+            fs.writeFileSync(destFile.replace('.ejs', '.html'), html);
+            console.log(`Rendered EJS file: ${file}`);
+        } else {
+            fs.copyFileSync(srcFile, destFile);
+            console.log(`Copied file: ${file}`);
+        }
+    });
 }
 
-// Render all EJS files in the views directory
+// Render EJS files from views directory to build directory
 renderEjsFiles(viewsDir, buildDir);
-
-// Copy static assets to build directory
-function copyStaticAssets(srcDir, destDir) {
-  fs.readdirSync(srcDir).forEach(file => {
-    const srcPath = path.join(srcDir, file);
-    const destPath = path.join(destDir, file);
-
-    if (fs.lstatSync(srcPath).isDirectory()) {
-      if (!fs.existsSync(destPath)) {
-        fs.mkdirSync(destPath);
-        console.log(`Created directory: ${destPath}`);
-      }
-      copyStaticAssets(srcPath, destPath);
-    } else {
-      fs.copyFileSync(srcPath, destPath);
-      console.log(`Copied file: ${srcPath} to ${destPath}`);
-    }
-  });
-}
-
-copyStaticAssets(staticDir, buildDir);
