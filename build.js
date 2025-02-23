8a233ba921e39b5dfa4e1f3c6f21a5ed857d550c
krxnkos <thomaspullan07@gmail.com>
2025-02-23 12:51:39 +0000

    Deployment Mk2

--- build.js
+++ build.js
@@ -4,14 +4,26 @@ const ejs = require('ejs');
 
 const viewsDir = path.join(__dirname, 'views');
 const buildDir = path.join(__dirname, 'build');
+const partialsDir = path.join(viewsDir, 'partials');
+const buildPartialsDir = path.join(buildDir, 'partials');
 
 // Create build directory if it doesn't exist
 if (!fs.existsSync(buildDir)) {
   fs.mkdirSync(buildDir);
 }
 
+// Create build/partials directory if it doesn't exist
+if (!fs.existsSync(buildPartialsDir)) {
+  fs.mkdirSync(buildPartialsDir);
+}
+
+// Copy partials files to build/partials directory
+fs.readdirSync(partialsDir).forEach(file => {
+  fs.copyFileSync(path.join(partialsDir, file), path.join(buildPartialsDir, file));
+});
+
 // Render index.ejs to index.html
 const indexTemplate = fs.readFileSync(path.join(viewsDir, 'index.ejs'), 'utf-8');
-const indexHtml = ejs.render(indexTemplate, {});
+const indexHtml = ejs.render(indexTemplate, {}, { views: [viewsDir] });
 
 fs.writeFileSync(path.join(buildDir, 'index.html'), indexHtml);
