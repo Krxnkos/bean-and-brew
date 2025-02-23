957a0ef9584fc252ff23fe23a0b24f180c227af8
krxnkos <thomaspullan07@gmail.com>
2025-02-23 14:29:20 +0000

    Refactor build process to ensure partials are copied correctly and update navbar EJS template

--- build.js
+++ build.js
@@ -1,48 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const ejs = require('ejs');
 
-const buildDir = path.join(__dirname, 'build');
 const viewsDir = path.join(__dirname, 'views');
-const partialsDir = path.join(viewsDir, 'partials');
+const buildDir = path.join(__dirname, 'build', 'views');
 
-// Create build directory if it doesn't exist
-if (!fs.existsSync(buildDir)) {
-    fs.mkdirSync(buildDir);
+// Ensure partials are copied
+const partialsDir = path.join(viewsDir, 'partials');
+const buildPartialsDir = path.join(buildDir, 'partials');
+
+if (!fs.existsSync(buildPartialsDir)) {
+    fs.mkdirSync(buildPartialsDir, { recursive: true });
 }
 
-// Create partials directory in build directory
-const buildPartialsDir = path.join(buildDir, 'views', 'partials');
-fs.mkdirSync(buildPartialsDir, { recursive: true });
-
-// Copy partials to build directory
+// Copy partial files
 fs.readdirSync(partialsDir).forEach(file => {
-    const srcFile = path.join(partialsDir, file);
-    const destFile = path.join(buildPartialsDir, file);
-    fs.copyFileSync(srcFile, destFile);
+    fs.copyFileSync(path.join(partialsDir, file), path.join(buildPartialsDir, file));
     console.log(`Copied partial file: ${file}`);
 });
-
-// Function to render EJS files
-function renderEjsFiles(srcDir, destDir) {
-    fs.readdirSync(srcDir).forEach(file => {
-        const srcFile = path.join(srcDir, file);
-        const destFile = path.join(destDir, file);
-
-        if (fs.lstatSync(srcFile).isDirectory()) {
-            fs.mkdirSync(destFile, { recursive: true });
-            renderEjsFiles(srcFile, destFile);
-        } else if (path.extname(file) === '.ejs') {
-            const template = fs.readFileSync(srcFile, 'utf-8');
-            const html = ejs.render(template, {}, { filename: srcFile });
-            fs.writeFileSync(destFile.replace('.ejs', '.html'), html);
-            console.log(`Rendered EJS file: ${file}`);
-        } else {
-            fs.copyFileSync(srcFile, destFile);
-            console.log(`Copied file: ${file}`);
-        }
-    });
-}
-
-// Render EJS files from views directory to build directory
-renderEjsFiles(viewsDir, buildDir);
